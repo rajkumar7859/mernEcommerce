d586f2a0aab6cb8e1ace1dd6b6f4aff72d51d766
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2023-02-07 15:04:33 +0530

    navbar

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '400px',
+      'md': '750px',
+      'lg': '1440px',
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
