7115145f72b07b5bb8296e8e1b78155ae79c0bb8
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2023-02-14 17:46:31 +0530

    home

--- frontend/src/Components/ImageSlider.jsx
+++ frontend/src/Components/ImageSlider.jsx
@@ -1,128 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Autoplay, Pagination, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../Styles/Swiper.css";
 
 const ImageSlider = () => {
-  const images = [
-    'https://picsum.photos/id/1/500/300',
-    'https://picsum.photos/id/2/500/300',
-    'https://picsum.photos/id/3/500/300',
-    'https://picsum.photos/id/4/500/300',
-    'https://picsum.photos/id/5/500/300'
-  ];
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [autoPlay, setAutoPlay] = useState(true);
-
-  useEffect(() => {
-    let interval;
-    if (autoPlay) {
-      interval = setInterval(() => {
-        setCurrentIndex(currentIndex => (currentIndex + 1) % images.length);
-      }, 5000);
-    }
-    return () => clearInterval(interval);
-  }, [autoPlay]);
-
-  const handlePrev = () => {
-    setCurrentIndex(currentIndex => (currentIndex + images.length - 1) % images.length);
-  };
-
-  const handleNext = () => {
-    setCurrentIndex(currentIndex => (currentIndex + 1) % images.length);
-  };
-
-  const handleImageChange = (index) => {
-    setCurrentIndex(index);
-  };
+  
+    const images = [
+        {
+          url: "https://img.freepik.com/premium-photo/shopping-trolley-blue-background-with-some-copy-space_348487-393.jpg",
+        },
+        {
+          url: "https://picsum.photos/id/20/300/200",
+        },
+        {
+          url: "https://picsum.photos/id/30/300/200",
+        },
+      ];
 
   return (
-    <div
-  id="carouselDarkVariant"
-  class="carousel slide carousel-fade carousel-dark relative"
-  data-bs-ride="carousel"
->
-  {/* <!-- Indicators --> */}
-  <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-    <button
-      data-bs-target="#carouselDarkVariant"
-      data-bs-slide-to="0"
-      class="active"
-      aria-current="true"
-      aria-label="Slide 1"
-    ></button>
-    <button
-      data-bs-target="#carouselDarkVariant"
-      data-bs-slide-to="1"
-      aria-label="Slide 1"
-    ></button>
-    <button
-      data-bs-target="#carouselDarkVariant"
-      data-bs-slide-to="2"
-      aria-label="Slide 1"
-    ></button>
-  </div>
-
-  {/* <!-- Inner --> */}
-  <div class="carousel-inner relative w-full overflow-hidden">
-    {/* <!-- Single item --> */}
-    <div class="carousel-item active relative float-left w-full">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(19).webp"
-        class="block w-full"
-        alt="Motorbike Smoke"
-      />
-      <div class="carousel-caption hidden md:block absolute text-center">
-        <h5 class="text-xl">First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+    <div >
+       <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        // onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        {
+            images?.map((sliderImg)=>{
+              return <SwiperSlide><img src={sliderImg.url} alt="slider" /></SwiperSlide>
+            })
+        }
+       
+        {/* <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg> */}
+        {/* <span ref={progressContent}></span> */}
+        {/* </div> */}
+      </Swiper>
     </div>
-
-    {/* <!-- Single item --> */}
-    <div class="carousel-item relative float-left w-full">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(35).webp"
-        class="block w-full"
-        alt="Mountaintop"
-      />
-      <div class="carousel-caption hidden md:block absolute text-center">
-        <h5 class="text-xl">Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-
-    {/* <!-- Single item --> */}
-    <div class="carousel-item relative float-left w-full">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(40).webp"
-        class="block w-full"
-        alt="Woman Reading a Book"
-      />
-      <div class="carousel-caption hidden md:block absolute text-center">
-        <h5 class="text-xl">Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  {/* <!-- Inner -->
-
-  <!-- Controls --> */}
-  <button
-    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-    type="button"
-    data-bs-target="#carouselDarkVariant"
-    data-bs-slide="prev"
-  >
-    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button
-    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-    type="button"
-    data-bs-target="#carouselDarkVariant"
-    data-bs-slide="next"
-  >
-    <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
   );
 };
 
