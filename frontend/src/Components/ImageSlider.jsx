2de2bb605e012d4a61784c3c1ec318f9d50a0354
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2023-02-15 14:21:40 +0530

    slide

--- frontend/src/Components/ImageSlider.jsx
+++ frontend/src/Components/ImageSlider.jsx
@@ -35,7 +35,6 @@ const ImageSlider = () => {
         }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        // onAutoplayTimeLeft={onAutoplayTimeLeft}
         className="mySwiper"
       >
         {
@@ -43,13 +42,6 @@ const ImageSlider = () => {
               return <SwiperSlide><img src={sliderImg.url} alt="slider" /></SwiperSlide>
             })
         }
-       
-        {/* <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            <circle cx="24" cy="24" r="20"></circle>
-          </svg> */}
-        {/* <span ref={progressContent}></span> */}
-        {/* </div> */}
       </Swiper>
     </div>
   );
